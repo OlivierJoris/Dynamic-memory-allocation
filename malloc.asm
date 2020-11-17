7bfdacb4beb22270797a7b190ad7eb0f46006c23
Maxime Goffart <maximegoffart@icloud.com>
2020-11-17 19:39:11 +0100

    [MALLOC] V1

--- malloc.asm
+++ malloc.asm
@@ -21,14 +21,94 @@ bbp_init_val:
 |; Following macros are based on the ones of malloc.c
 
 |; stores in Reg[Ro] the address of another memory block
-|; from memory block at Mem[pointer]
-.macro block_next(pointer, Ro) ADDC(pointer, 4, Ro) LD(Ro, 0, Ro)
-|; stores in Reg[Ro] the size of memory block at Mem[pointer]
-.macro block_size(pointer, Ro) LD(pointer, 0, Ro)
+|; from header of memory block at Mem[Reg[Ra]]
+.macro block_next_get(Ra, Ro) LD(Ra, 4, Ro)
+|; stores in Reg[Ro] the size of memory block at Mem[Reg[Ra]]
+.macro block_size_get(Ra, Ro) LD(Ra, 0, Ro)
 |; stores in Reg[Ro] the address of the first word of the
-|; chunk of memory of memory block at Mem[pointer]
-.macro block_start(pointer, Ro) ADDC(pointer, 8, Ro)
+|; chunk of memory of memory block at Mem[Reg[Ra]]
+.macro block_start(Ra, Ro) ADDC(Ra, 8, Ro)
 
+|; stores content of Reg[Rb] as next block address of
+|; block at Mem[Reg[Ra]]
+.macro block_next_set(Ra, Rb) ST(Rb, 4, Ra)
+|; stores content of Reg[Rb] as block size of block at Mem[Reg[Ra]]
+.macrp block_size_set(Ra, Rb) ST(Rb, 0, Ra)
+
+
+|; Checks if a given block can hold a given space.
+|; Updates the free list if the block if valid.
+|; Slip if necessary the given block if its size exceeds the
+|;   given size.
+|; Args:
+|;  - n Requested size
+|;  - curr Pointer to header of current block
+|;  - prev Pointer to header of previous block (may be NULL)
+|; Returns:
+|;  - 1 if the block was used
+|;  - Else, 0
+try_use_block:
+	PUSH(LP) PUSH(BP)
+	MOVE(SP, BP)
+
+	|; stores registers that will be used to load args
+	PUSH(R1) PUSH(R2) PUSH(R3)
+	|; stores registers that will be modified
+	PUSH(R4) PUSH(R5) PUSH(R6) PUSH(R7)
+
+	|; gets arguments from stack
+	LD(BP, -12, R1) |; n
+	LD(BP, -16, R2) |; curr
+	LD(BP, -20, R3) |; prev
+
+	|; sets registers
+	block_next_get(R2, R4) 			|; R4 <- next
+	block_size_get(R2, R5) 			|; R5 <- block_size(curr)
+	ADDC(R1, 2, R0)
+	MOVE(R0, R6) 					|; R6 <- n+2
+
+	|; checks if block_size(curr) is larger than n
+	CMPLE(R6, R5, R0) 				|; R5 >= R6 <=> R6 <= R5
+	BF(R0, try_use_block_same_size)
+	ADD(R2, R6, R7) 				|; R7 <- new_block
+	block_next_set(R7, R4)			|; set block next of new block
+	SUB(R5, R6, R0)
+	block_size_set(R7, R0)			|; set block size of new block
+	MOVE(R7, R4)					|; next <- new_block
+	BR(try_use_block_updates)
+
+try_use_block_same_size:
+	|; block_size(curr) ?= n
+	CMPEQ(R5, R1, R0)
+	BF(R0, try_use_block_smaller)
+	BR(try_use_block_updates)
+
+try_use_block_updates:
+	|; removes block from free list & updates header
+	block_size_set(R2, R1) 			|; size curr = n
+	CMOVE(NULL, R0)
+	block_next_set(R2, R0) 			|; next curr = NULL
+	CMPEQC(R3, NULL, R0)
+	BF(try_use_block_set_fp)
+	block_next_set(R3, R4)
+	CMOVE(1, R0)
+	BR(try_use_block_end)
+
+try_use_block_set_fp:
+	MOVE(R4, FP)
+	CMOVE(1, R0)
+	BR(try_use_block_end)
+
+try_use_block_smaller:
+	|; block_size(curr) < n
+	CMOVE(0, R0)
+	BR(try_use_block_end)
+
+try_use_block_end:
+	POP(R7) POP(R6) POP(R5) POP(R4)
+	pop(R3) POP(R2) POP(R1)
+	POP(BP) POP(LP)
+	RTN()
 
 
 |; Dynamically allocates an array of size n.
@@ -41,31 +121,67 @@ malloc:
 	MOVE(SP, BP)
 	|; Insert your malloc implementation here ....
 
-	|; Stores registers that will be modified
-	PUSH (R1) |; n
-	PUSH (R2) |; prev
-	PUSH (R3) |; curr
+	|; stores registers that will be modified
+	PUSH(R1) |; n
+	PUSH(R2) |; prev
+	PUSH(R3) |; curr
+	PUSH(R4) |; n_items
+	PUSH(R5) |; new_bbp
+	PUSH(R6) |; first condition
+	PUSH(R7) |; second condition
 
-	|; Gets argument n from stack
+	|; gets argument n from stack
 	LD(BP, -12, R1)
 	CMPLEC(R1, 0, R0)
-	BT(RO, malloc_error) |; n <= 0
+	BT(R0, malloc_error) |; n <= 0
 
 	|; sets pointers prev and curr
 	CMOVE(NULL, R2)
-	CMOVE(FP, R3)
+	MOVE(FP, R3)
 
+malloc_loop:
+	CMPEQC(R3, NULL, R0)
+	BT(R0, malloc_no_valid_block)
+	|; push arguments before calling try_use_block
+	PUSH(R2)
+	PUSH(R3)
+	PUSH(R1)
+	CALL(try_use_block, 3)
+	CMPEQC(R0, 1, R0)
+	BF(R0, malloc_loop_next_iteration)
+	block_start(R3, R0)
+	BR(malloc_end)
 
+malloc_loop_next_iteration:
+	|; modifies registers for next iteration
+	MOVE(R3, R2)			|; pev = curr
+	block_next_get(R3, R3)	|; curr = block_next(curr)
+	BR(malloc_loop)
 
-malloc_end:
-	POP(R1)
-	RTN()
+malloc_no_valid_block:
+	|; need to create a new block
+	ADDC(R1, 2, R4)
+	SUB(BBP, R4, R5)
+	CMPLE(R5, SP, R6)
+	CMPLE(BBP, R5, R7)
+	OR(R6, R7, R0)
+	BT(R0, malloc_error)
+	SUB(BBP, R4, BBP)
+	CMOVE(NULL, R0)
+	block_next_set(BBP, R0)
+	block_size_set(BBP, R1)
+	block_start(BBP, R0)
+	BR(malloc_end)
 
-|; Error in malloc
 malloc_error:
 	CMOVE(NULL, R0)
-	RTN()
+	BR(malloc_end)
 
+malloc_end:
+	POP(R7) POP(R6) POP(R5) POP(R4)
+	POP(R3) POP(R2) POP(R1)
+	POP(BP) POP(LP)
+	RTN()
 
 
 |; Free a dynamically allocated array starting at address p.
